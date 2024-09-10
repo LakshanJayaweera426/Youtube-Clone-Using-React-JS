25f214421861a0306139665e1defcdb5a453380a
Lakshan Jayaweera <127861567+LakshanJayaweera426@users.noreply.github.com>
2024-09-11 00:55:05 +0530

    Developed Feed.jsx and Feed.css files

--- youtube-clone/src/App.jsx
+++ youtube-clone/src/App.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/navbar/Navbar'
 import { Routes,Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Video from './pages/video/Video'
 
 const App = () => {
+
+  const [sidebar,setSidebar] = useState(true);
+
   return (
     <div>
-      <Navbar/>
+      <Navbar setSidebar={setSidebar}/>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/' element={<Home sidebar={sidebar} />} />
         <Route path='/video/:categoryId/:videoId' element={<Video/>} />
       </Routes>
     </div>
